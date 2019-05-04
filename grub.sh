56f8a9433ea6bfcbbd596bed58efdba73ed037ba
lamhaoyin2 <46623537+lamhaoyin2@users.noreply.github.com>
2019-05-04 15:47:07 +0800

    Update grub.sh

--- grub.sh
+++ grub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT="\ cgroup_enable=memory\ swapaccount=1/' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT="\ cgroup_enable=memory\ swapaccount=1\ /' /etc/default/grub
 update-grub && reboot
